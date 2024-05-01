06149d64b8259fbca5a4632d1d4ebbc8b5c7de32
dafandikri <dafandikri@gmail.com>
2024-05-01 23:50:11 +0700

    Fixed Bugs at CreditCardPayment

--- assignment3/src/main/java/assignments/assignment3/payment/CreditCardPayment.java
+++ assignment3/src/main/java/assignments/assignment3/payment/CreditCardPayment.java
@@ -3,7 +3,7 @@ package assignments.assignment3.payment;
 public class CreditCardPayment implements DepeFoodPaymentSystem{
     //TODO: implementasikan class yang implement interface di sini
     // Anda dibebaskan untuk membuat method yang diperlukan
-    public final double TRANSACTION_FEE_PERCENTAGE = 0.2;
+    public final double TRANSACTION_FEE_PERCENTAGE = 0.02;
 
     public long countTransactionFee(long amount){
         return (long) (amount * TRANSACTION_FEE_PERCENTAGE);
