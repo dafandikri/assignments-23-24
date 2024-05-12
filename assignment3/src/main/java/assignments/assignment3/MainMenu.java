c913151ea6739ccd6c5d98a9c2bb3b1d3a3029ef
dafandikri <dafandikri@gmail.com>
2024-05-12 12:20:50 +0700

    fixed depefood

--- assignment3/src/main/java/assignments/assignment3/MainMenu.java
+++ assignment3/src/main/java/assignments/assignment3/MainMenu.java
@@ -134,7 +134,6 @@ public class MainMenu {
         userList.add(new User("Aurora Anum", "087788129043", "a.anum@gmail.com", "U", "Customer", new DebitPayment(), 650000));
 
         userList.add(new User("Admin", "123456789", "admin@gmail.com", "-", "Admin", new CreditCardPayment(), 0));
-        userList.add(
-                new User("Admin Baik", "9123912308", "admin.b@gmail.com", "-", "Admin", new CreditCardPayment(), 0));
+        userList.add(new User("Admin Baik", "9123912308", "admin.b@gmail.com", "-", "Admin", new CreditCardPayment(), 0));
     }
 }
